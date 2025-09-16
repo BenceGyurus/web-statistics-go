2e7686dc60839f927c245c0ebdd2f5c6c7a384af
Gyürüs Bence <81524938+BenceGyurus@users.noreply.github.com>
2025-09-16 12:41:16 +0200

    icon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,23 @@
-import Head from "next/head";
-import "./globals.css";
+// app/layout.tsx
+import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Oldal statisztikák',
+  description: 'A bnbdevelopment oldalainak statisztikái',
+  icons: {
+    icon: '/logo.png',
+  },
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="hu">
-      <Head>
-        <link rel="icon" href="/logo.png" sizes="any" />
-      </Head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
