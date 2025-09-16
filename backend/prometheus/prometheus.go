cd89085aa224c314f20907a238219e83b9e5a191
Bence Toth <36576531+bencetotht@users.noreply.github.com>
2025-09-16 12:06:18 +0200

    group by sites

--- backend/prometheus/prometheus.go
+++ backend/prometheus/prometheus.go
@@ -1,7 +1,9 @@
 package prometheus
 
 import (
+	"statistics/database"
 	"statistics/statistics"
+	"statistics/structs"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -9,26 +11,42 @@ import (
 )
 
 var (
-	visitors = promauto.NewGauge(prometheus.GaugeOpts{
+	visitorsBySite = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "statistics_traffic",
-		Help: "Number of traffic",
-	})
-	active_users = promauto.NewGauge(prometheus.GaugeOpts{
+		Help: "Unique sessions (traffic) in last 5 minutes by site",
+	}, []string{"site"})
+	activeUsersBySite = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "statistics_active_users",
-		Help: "Number of currently active users",
-	})
-	minuteSpentGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Help: "Number of currently active users by site (last 5 minutes)",
+	}, []string{"site"})
+	minuteSpentBySite = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "statistics_minute_spent",
-		Help: "Number of minute spent",
-	})
+		Help: "Average minutes spent on site in last 24h by site",
+	}, []string{"site"})
 )
 
 func RecordMetrics() {
 	go func() {
 		for {
-			visitors.Set(float64(statistics.GetUsers(time.Now().Add(-5*time.Minute), time.Now(), "")))
-			active_users.Set(float64(statistics.ActiveUsers("")))
-			minuteSpentGauge.Set(float64(statistics.TimeOnSite("", time.Now().Add(-24*time.Hour), time.Now())))
+			// Fetch distinct sites
+			var sites []string
+			_ = database.Session.Model(&structs.WebMetric{}).Distinct("site").Pluck("site", &sites).Error
+
+			// Update metrics per site
+			for _, site := range sites {
+				if site == "" {
+					continue
+				}
+				visitorsBySite.With(prometheus.Labels{"site": site}).Set(
+					float64(statistics.GetUsers(time.Now().Add(-5*time.Minute), time.Now(), site)),
+				)
+				activeUsersBySite.With(prometheus.Labels{"site": site}).Set(
+					float64(statistics.ActiveUsers(site)),
+				)
+				minuteSpentBySite.With(prometheus.Labels{"site": site}).Set(
+					float64(statistics.TimeOnSite(site, time.Now().Add(-24*time.Hour), time.Now())),
+				)
+			}
 			time.Sleep(2 * time.Second)
 		}
 	}()
